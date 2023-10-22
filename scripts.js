d4abe6a2c85d7d7bdc72b15cd1453535cf9a872c
hkursatakburak <akburakkursat54@gmail.com>
2023-10-22 20:38:18 +0300

    added parallax and responsive footer

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 let text = document.getElementById('text');
-let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 let navbarset = document.getElementById('navbarset');
@@ -12,5 +11,4 @@ window.addEventListener('scroll', () => {
     navbarset.style.marginTop = value * -2.5 + 'px';
     hill5.style.left = value * -1.5 + 'px';
     hill4.style.left = value * 1.5 + 'px';
-    hill1.style.top = value * 1 + 'px';
 })
